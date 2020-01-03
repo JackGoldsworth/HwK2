90d62e4c63e62dc5ba8155cfcc9cafa15ec2c084
Jack Goldsworth <JackGoldsworth@gmail.com>
2020-01-03 17:52:57 -0600

    some maths.

--- src/main/kotlin/me/jackgoldsworth/hwk2/visitor/ExpressionVisitor.kt
+++ src/main/kotlin/me/jackgoldsworth/hwk2/visitor/ExpressionVisitor.kt
@@ -7,6 +7,7 @@ import me.jackgoldsworth.hwk2.domain.expression.Value
 import me.jackgoldsworth.hwk2.domain.expression.VariableReference
 import me.jackgoldsworth.hwk2.parser.HwKBaseVisitor
 import me.jackgoldsworth.hwk2.parser.HwKParser
+import kotlin.math.pow
 
 class ExpressionVisitor(private val scope: Scope) : HwKBaseVisitor<Expression>() {
 
@@ -19,5 +20,35 @@ class ExpressionVisitor(private val scope: Scope) : HwKBaseVisitor<Expression>()
 
     override fun visitValue(ctx: HwKParser.ValueContext): Expression {
         return Value(Type.getTypeFromValue(ctx.text), ctx.text)
+    }
+
+    override fun visitADD(ctx: HwKParser.ADDContext): Expression {
+        val firstVal = ctx.expression(0).text
+        val secondVal = ctx.expression(1).text
+        return Value(Type.INT, (firstVal.toInt() + secondVal.toInt()).toString())
+    }
+
+    override fun visitSUB(ctx: HwKParser.SUBContext): Expression {
+        val firstVal = ctx.expression(0).text
+        val secondVal = ctx.expression(1).text
+        return Value(Type.INT, (firstVal.toInt() - secondVal.toInt()).toString())
+    }
+
+    override fun visitMULT(ctx: HwKParser.MULTContext): Expression {
+        val firstVal = ctx.expression(0).text
+        val secondVal = ctx.expression(1).text
+        return Value(Type.INT, (firstVal.toInt() * secondVal.toInt()).toString())
+    }
+
+    override fun visitDIV(ctx: HwKParser.DIVContext): Expression {
+        val firstVal = ctx.expression(0).text
+        val secondVal = ctx.expression(1).text
+        return Value(Type.INT, (firstVal.toInt() / secondVal.toInt()).toString())
+    }
+
+    override fun visitEXP(ctx: HwKParser.EXPContext): Expression {
+        val firstVal = ctx.expression(0).text
+        val secondVal = ctx.expression(1).text
+        return Value(Type.INT, firstVal.toDouble().pow(secondVal.toDouble()).toInt().toString())
     }
 }
